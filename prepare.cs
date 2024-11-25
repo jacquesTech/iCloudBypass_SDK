7d716c995491073a088eff234969e1bd12c115c3
GSM ADJAA <96638829+jacquesTech@users.noreply.github.com>
2024-11-25 18:38:04 +0100

    Create prepare.cs
    Data Preparation

---
+++ prepare.cs
@@ -0,0 +1,35 @@
+using adjaassh;
+using adjaamobiledevice;
+using System.IO;
+
+
+using namespace iCloudBypass_SDK
+{
+
+public string device {get , set};
+public class prepare : iCloudBypass_SDK
+{
+ public bool preparedata()
+ {
+   bool result = false;
+     using (WebClient icloud = new WebClient())
+{
+       try
+       { 
+         if(icloud.DownloadString("").Contains(""))
+  {
+
+    
+  }
+       }
+       catch(Exception errorx)
+       {
+         result = false;
+       }
+ return result;
+}  
+ }
+  
+}
+  
+}
